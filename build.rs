14f669ebe3a56743b5e3d47bf98040c429b32a45
Robert Węcławski <r.weclawski@gmail.com>
2021-06-03 21:54:02 +0200

    add generation of AppImage

--- build.rs
+++ build.rs
@@ -6,16 +6,41 @@ fn main() {
         .status()
         .unwrap();
 
+    let python_installed = Command::new("sh")
+        .args(&["-c", "command -v python3"])
+        .status()
+        .unwrap()
+        .success();
+    let pip_installed = Command::new("sh")
+        .args(&["-c", "command -v pip3"])
+        .status()
+        .unwrap()
+        .success();
+    let wget_installed = Command::new("sh")
+        .args(&["-c", "command -v wget"])
+        .status()
+        .unwrap()
+        .success();
 
-    let python_installed = Command::new("sh").args(&["-c", "command -v python3"]).status().unwrap().success();
-    let pip_installed = Command::new("sh").args(&["-c", "command -v pip3"]).status().unwrap().success();
-    let wget_installed = Command::new("sh").args(&["-c", "command -v wget"]).status().unwrap().success();
-    
     if python_installed && pip_installed && wget_installed {
-        Command::new("pip3").args(&["install", "aiohttp", "toml"]).status().unwrap();
+        Command::new("pip3")
+            .args(&["install", "aiohttp", "toml"])
+            .status()
+            .unwrap();
         Command::new("wget").arg("https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/master/cargo/flatpak-cargo-generator.py").status().unwrap();
-        Command::new("python3").args(&["flatpak-cargo-generator.py", "Cargo.lock", "-o", "src/resources/cargo-sources.json"]).status().unwrap();
-        Command::new("rm").arg("flatpak-cargo-generator.py").status().unwrap();
+        Command::new("python3")
+            .args(&[
+                "flatpak-cargo-generator.py",
+                "Cargo.lock",
+                "-o",
+                "src/resources/cargo-sources.json",
+            ])
+            .status()
+            .unwrap();
+        Command::new("rm")
+            .arg("flatpak-cargo-generator.py")
+            .status()
+            .unwrap();
     }
 
     println!("cargo:rerun-if-changed=src/resources/resources.xml");
