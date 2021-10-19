e0b5f8b46819bd8939fc6d022cb83c8f73b487b9
Robert Węcławski <r.weclawski@gmail.com>
2021-10-20 00:30:35 +0200

    don't use gsettings if gsettings for app are not present

--- src/settings.rs
+++ src/settings.rs
@@ -1,36 +1,54 @@
 use gtk::gio;
 use gtk::gio::prelude::SettingsExt;
+use gtk::gio::SettingsSchemaSource;
 
 use crate::image::PreviewSize;
 
 #[derive(Clone)]
 pub struct Settings {
-    settings: gio::Settings,
+    gio_settings: Option<gio::Settings>,
     scale: PreviewSize,
 }
 
 impl Settings {
     pub fn new(application_id: &str) -> Settings {
+        let gio_settings = match SettingsSchemaSource::default() {
+            Some(schema_source) => {
+                if schema_source.lookup(application_id, true).is_some() {
+                    Some(gio::Settings::new(application_id))
+                } else {
+                    None
+                }
+            }
+            None => None,
+        };
+
         Settings {
-            settings: gio::Settings::new(application_id),
+            gio_settings,
             scale: PreviewSize::BestFit(0, 0),
         }
     }
 
     pub fn set_window_size(&self, window_size: (u32, u32)) {
-        let (window_width, window_height) = window_size;
-        self.settings
-            .set_uint("window-width", window_width)
-            .expect("Could not set setting window-width.");
-        self.settings
-            .set_uint("window-height", window_height)
-            .expect("Could not set setting window-height.");
+        if let Some(gio_settings) = self.gio_settings.as_ref() {
+            let (window_width, window_height) = window_size;
+            gio_settings
+                .set_uint("window-width", window_width)
+                .expect("Could not set setting window-width.");
+            gio_settings
+                .set_uint("window-height", window_height)
+                .expect("Could not set setting window-height.");
+        }
     }
 
     pub fn window_size(&self) -> (u32, u32) {
-        let window_width = self.settings.uint("window-width");
-        let window_height = self.settings.uint("window-height");
-        (window_width, window_height)
+        match self.gio_settings.as_ref() {
+            Some(gio_settings) => (
+                gio_settings.uint("window-width"),
+                gio_settings.uint("window-height"),
+            ),
+            None => (1024, 768),
+        }
     }
 
     pub fn set_scale(&mut self, preview_size: PreviewSize) {
